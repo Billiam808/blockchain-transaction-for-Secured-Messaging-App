7bfbe001ec9a5004ab642f6e99a7fd6b193274ca
Bill Adona <bill.adona08@gmail.com>
2016-10-27 20:01:19 -0600

    Update Block.java

--- src/Block.java
+++ src/Block.java
@@ -3,34 +3,101 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package blockchain_transaction;
+import java.util.*;
+import java.sql.Timestamp;
 
 /**
  *
  * @author BillJoshua
  */
-public class Block {
-    Block prev;
-    String timeStamp;
-    double price;
-    String buyer;
-    String seller;
+
+public final class Block                                                                // Class is final so no one can create a subclass of it and create their own function
+{
+        private Block prev;
+        private Timestamp stamp;
+        private double amount;
+        private String buyerID;
+        private String sellerID;
     
-    // constructor
-    Block()
-    {
-        prev = null;
-        timeStamp = null;
-        price = 0.0;
-        buyer = null;
-        seller = null;
-    }
     
-    Block(Block p, String ts, double pr, String b, String s)
-    {
-        prev = p;
-        timeStamp = ts;
-        price = pr;
-        buyer = b;
-        seller = s;
-    }
+        // getters
+        public Block getPrev()
+        {
+            return prev;
+        }
+        public Timestamp getTimeStamp()
+        {
+            return stamp;
+        }
+        public double getAmount()
+        {
+            return amount;
+        }
+        public String getBuyerID()
+        {
+            return buyerID;
+        }
+        public String getSellerID()
+        {
+            return sellerID;
+        }
+        public boolean isEqual(Block b)
+        {
+            return stamp.equals(b.getTimeStamp());
+        }
+        public boolean isBefore(Block b)
+        {
+            return stamp.before(b.getTimeStamp());
+        }
+        
+        // modifiers
+        public void setPrev(Block p)
+        {
+            prev = p;
+        }
+  
+        public void setTimeStamp()
+        {
+            Calendar calendar = Calendar.getInstance();
+            Date now = calendar.getTime();
+            stamp = new Timestamp(now.getTime());
+        }
+ 
+        public void setBuyerID(String b_id)
+        {
+            buyerID = b_id;
+        }
+        
+        public void setSellerID(String s_id)
+        {
+            sellerID = s_id;
+        }
+        
+        public void setAmount(double amnt)
+        {
+            amount = amnt;
+        }
+            
+                
+        // constructor
+        public Block()
+        {
+            prev = null;
+            stamp = null;
+            buyerID = null;
+            sellerID = null;
+            amount = 0;
+                    
+        }
+
+        // constructor with parameters
+        public Block(Block p, double amount1, String buy_ID, String sell_ID)
+        {
+            prev = p;
+            amount = amount1;
+            buyerID = buy_ID;
+            sellerID = sell_ID;
+            setTimeStamp();
+        }
 }
